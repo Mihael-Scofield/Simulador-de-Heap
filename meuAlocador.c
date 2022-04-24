65a8d2854b52ba85814c34159376f8b9d9427aa5
Mihael Scofield <42939750+Mihael-Scofield@users.noreply.github.com>
2022-04-23 23:38:05 -0300

    Correções de Representação

--- meuAlocador.c
+++ meuAlocador.c
@@ -1,135 +1,146 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int64_t* topoInicialHeap;
-int64_t* heap;
-int64_t* enderecoInicialBusca;
-int64_t* enderecoBusca;
+int topoInicialHeap; // Seria um endereço int64_t, 8 bytes, porém aqui será apenas o índice 0
+int enderecoInicialBusca; // para o ponteiro das buscas, utilizaremos a mesma lógica
+int enderecoBusca;
+int brk; // indice que simula o que seria a o endereco final da "heap"
+int64_t* heapHipotetica;
 
-int64_t iniciaAlocador() {
-    heap = malloc(sizeof(int64_t) * 10000);
-    topoInicialHeap = heap;
-    printf("\n %p \n", topoInicialHeap);
+void iniciaAlocador() {
+    heapHipotetica = malloc(sizeof(int64_t) * 10000); /// tamanho "estático", apenas para teste
+    brk = 0; // Seria o endereco, mas como estamos usando indices é o 0
+    topoInicialHeap = brk; 
 }
 
-int64_t finalizaAlocador() {
-    heap = topoInicialHeap;
+void finalizaAlocador() {
+    // brk = topoInicialHeap; // Para liberar o endereço utilizariamos essa linha
+    free(heapHipotetica);
 }
 
-int liberaMem(int64_t* bloco) {
-    bloco = bloco - 2;
-    *bloco = 0;
+void liberaMem(int bloco) {
+    heapHipotetica[bloco - 2] = 0; // Volta "8 bytes" (2 enderecos) e seta como livre, isto e, 0
 }
 
-int64_t* busca(int num_bytes) {
-    int flag = 0;    
+int busca(int num_bytes) {
+    int flag = 0;
     int num_bytesAUX;
+    enderecoBusca = enderecoInicialBusca; // considerado que endereco aponta para o bloco[2]
     
-    enderecoBusca = enderecoInicialBusca;
     do {
-        // Pula para próximo bloco
-        num_bytesAUX = *(enderecoBusca) - 1;
-        enderecoBusca = enderecoBusca + num_bytesAUX;
+        /* Pula para próximo bloco */
+        num_bytesAUX = heapHipotetica[enderecoBusca - 1]; // pega a quantidade de indices a pular
+        enderecoBusca = enderecoBusca + num_bytesAUX + 2;
 
-        // Verifica se deve voltar ao inicio
-        if(enderecoBusca > heap) {
-            enderecoBusca = topoInicialHeap + 2;
+        /* Verifica se deve voltar ao inicio */
+        if(enderecoBusca >= brk) {
+            enderecoBusca = topoInicialHeap + 2; // + 2 para considerar o comentario da linha 29
         }
 
-        // Investiga bloco atual
-        num_bytes = *(enderecoBusca) - 2;
-        if(num_bytes == 0) { // Bloco atual esta livre
-            num_bytes = *(enderecoBusca) - 1;
-            if(num_bytesAUX >= num_bytes) {
+        /* Investiga bloco atual */
+        // Verifica se bloco atual esta livre, reaproveitando num_bytes
+        if(heapHipotetica[enderecoBusca - 2] == 0) { // Bloco atual esta livre
+            // Verifica agora o tamanho do bloco atual
+            num_bytesAUX = heapHipotetica[enderecoBusca - 1];
+            if(num_bytes <= num_bytesAUX) {
                 flag = 1;
             }
         }
-    } while ((flag == 0) && (*enderecoBusca =! *enderecoInicialBusca));
+    } while ((flag == 0) && (enderecoBusca =! enderecoInicialBusca));
 
+    // caso nao tenha encontrado
     if((flag == 0) || (enderecoBusca == enderecoInicialBusca)) {
-        return 0;      
+        return 0;
     }
+
     return enderecoBusca;
 }
 
-int64_t* alocaMem(int num_bytes) {
-    // Alocacao no primeiro caso
-    if(heap == topoInicialHeap) {
-        *heap = 1;
-        heap = heap + 1;
-        *heap = num_bytes;
-        heap = heap + 1;
-        enderecoInicialBusca = heap;
-        heap = heap + num_bytes;
-        return enderecoInicialBusca; // retorna ultima posicao encontrada
+int alocaBloco(int enderecoBloco, int num_bytes) {
+    heapHipotetica[enderecoBloco - 2] = 1;
+    heapHipotetica[enderecoBloco - 1] = num_bytes;
+    enderecoInicialBusca = enderecoBloco;
+}
+
+int alocaMem(int num_bytes) {
+    /* Heap Vazia */
+    if(brk == topoInicialHeap) {
+        alocaBloco(2, num_bytes);
+        brk = brk + num_bytes + 2;
     } 
-    // Heap nao esta vazia, iremos buscar
+    /* Heap nao esta vazia, iremos buscar um bloco vazio >= em tamanho */
     else {
-        int64_t* novoEndereco = busca(num_bytes);
-        // Encontrou um bloco elegivel na busca e o trata
+        int novoEndereco = busca(num_bytes);
+        /* Caso em que encontrou um bloco elegivel na busca*/
         if (novoEndereco != 0) {
-            int num_bytesAUX = *(novoEndereco - 1);
-            *(novoEndereco - 2) = 1;
-            *(novoEndereco - 1) = num_bytes;
+            int num_bytesAUX = heapHipotetica[novoEndereco - 1];
+            alocaBloco(novoEndereco, num_bytes);
             // Verifica se ha a necessidade de "repartir" o bloco atual
             if(num_bytesAUX != num_bytes) {
-                novoEndereco = novoEndereco + num_bytes + 2;
-                *(novoEndereco - 2) = 0;
-                *(novoEndereco - 1) = num_bytesAUX - num_bytes;
+                // Avanca-se ate os blocos de controle da parte dividida, e posiciona os num_bytes que sobraram
+                alocaBloco(novoEndereco + num_bytes + 2, num_bytesAUX - num_bytes);
             }
         }
-        // nao encontrou, aumenta a heap
+        /* nao encontrou, aumenta a heap */
         else {
-            *heap = 1;
-            heap = heap + 1;
-            *heap = num_bytes;
-            heap = heap + 1;
-            enderecoInicialBusca = heap;
-            heap = heap + num_bytes;
-            return enderecoInicialBusca; // retorna ultima posicao encontrada
+            alocaBloco(brk + 2, num_bytes); // + 3 pois quero ignorar o ultimo espaco do ultimo bloco
+            brk = brk + num_bytes + 2; 
         }
     }
+    return enderecoInicialBusca;
 }
 
-int64_t imprimeMapa() {
+int64_t imprimeMapa(int chamada) {
+    printf("COMECANDO MAPA NUMERO %d", chamada); // DEBUGACAO DE ALUNO
     int flag, num_bytesAtual;
     char positividade;
-    int64_t* enderecoAtual = topoInicialHeap; // iniciamos no topo sempre
+    int enderecoAtual = topoInicialHeap + 2 ; // iniciamos no bloco do topo sempre
     
-    while(*enderecoAtual != *heap) {
-        // Descubro se a ocupação do bloco atual
-        flag = *(topoInicialHeap);
-        if (flag == 0) {
-            positividade = '-';
+    if (brk != 0) {
+        while(enderecoAtual < brk) {
+            // Descubro se a ocupação do bloco atual
+            flag = heapHipotetica[enderecoAtual - 2];
+            if (flag == 0) {
+                positividade = '-';
+            }
+             else {
+                positividade = '+';
+            }
+            num_bytesAtual = heapHipotetica[enderecoAtual - 1];
+            printf("\n ##");
+            for (int i = 0; i < num_bytesAtual; i++) {
+                printf("%c", positividade);
+            }
+            printf("\n");
+            enderecoAtual = enderecoAtual + num_bytesAtual + 2; // + 2 para pularmos o controle da frente
         }
-         else {
-            positividade = '+';
-        }
-        num_bytesAtual = *(enderecoAtual + 1);
-        printf("##");
-        for (int i = 0; i < num_bytesAtual; i++) {
-            printf("%c", positividade);
-        }
-        enderecoAtual = enderecoAtual + num_bytesAtual + 1; // + 1 para pularmos o controle da frente
     }
-
+    else  {
+        printf("\n Heap Vazia \n");
+    }
 }
 
-int64_t main() {
+int main() {
     printf("\n Olá BRK! É PRA FICAR PARADA OUVIU? \n");
 
-    int64_t *a, *b;
+    int a, b;
     iniciaAlocador();
-    imprimeMapa();
+    imprimeMapa(0);
 
-    a = alocaMem(240);
-    imprimeMapa();
-    b = alocaMem(50);
-    imprimeMapa();
-    liberaMem(a);
-    imprimeMapa();
-    a = alocaMem(50);
-    imprimeMapa();
+    a = alocaMem(10);
+    imprimeMapa(1);
+
+    b = alocaMem(5);
+    imprimeMapa(2);
+
+    int c = alocaMem(1);
+    imprimeMapa(3);
+
+    liberaMem(b);
+    imprimeMapa(4);
+
+    b = alocaMem(5);
+    imprimeMapa(5);
 
     finalizaAlocador();
 }
